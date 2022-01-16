2d2575ae98de45285fb2afcf2f5bd8840e5b26bb
sksat <sksat@arkedgespace.com>
2022-01-16 21:26:08 +0900

    fix sed markdown files only

--- prepro.sh
+++ prepro.sh
@@ -6,5 +6,5 @@ cp -r Specifications src/
 cp -r Tutorials src/
 
 # mathjax
-find src -type f -exec sed -i -e 's/$`/\\\\( /g' -e 's/`\$/ \\\\)/g' {} \;
-find src -type f -exec sed -i -z -e 's/```math\n\([^`]*\)```\n/\\\\[\n\1\\\\]\n/g' {} \;
+find src -type f -name '*.md' -exec sed -i -e 's/$`/\\\\( /g' -e 's/`\$/ \\\\)/g' {} \;
+find src -type f -name '*.md' -exec sed -i -z -e 's/```math\n\([^`]*\)```\n/\\\\[\n\1\\\\]\n/g' {} \;
