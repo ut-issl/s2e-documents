ffacc1aa8c0bc3e0033df0b32d86344e55778345
sksat <sksat@arkedgespace.com>
2022-01-17 12:56:32 +0900

    fix equation

--- prepro.sh
+++ prepro.sh
@@ -8,3 +8,6 @@ cp -r Tutorials src/
 # mathjax
 find src -type f -name '*.md' -exec sed -i -e 's/$`/\\\\( /g' -e 's/`\$/ \\\\)/g' {} \;
 find src -type f -name '*.md' -exec sed -i -z -e 's/```math\n\([^`]*\)```\n/\\\\[\n\1\\\\]\n/g' {} \;
+# escape in block equation
+find src -type f -name '*.md' -exec sed -i -z -e '/\\\\\[/,/\\\\\]/s/\\\\\n/\\\\\\\\\n/g' {} \;
+find src -type f -name '*.md' -exec sed -i -z -e '/\\\\\[/,/\\\\\]/s/}\_{/}\\_{/g' {} \;
