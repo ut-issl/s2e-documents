38c835cf27d176fffe39c974bbf98ec9e32a0d02
satoshi_ikari <ikari@space.t.u-tokyo.ac.jp>
2022-05-01 21:20:07 +0900

    modify sample codes for C2A integration

---
+++ Tutorials/SampleCodes/C2A_Integration/C2A_src_user/Applications/DriverInstances/di_s2e_uart_test.h
@@ -0,0 +1,12 @@
+/**
+ * @file   di_s2e_uart_test.h
+ * @brief  S2Eを使ったUART通信テスト用アプリ
+ */
+#ifndef DI_S2E_UART_TEST_H_
+#define DI_S2E_UART_TEST_H_
+
+#include <src_core/System/ApplicationManager/app_info.h>
+
+AppInfo S2E_UART_TEST_update(void);
+
+#endif
